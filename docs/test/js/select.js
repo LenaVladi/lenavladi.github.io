d27728ab53c6f500f73f341e93e0a12c88d10f4e
Siprikova Alena <asiprikova@ozon.ru>
2019-10-21 15:14:13 +0300

    babel js

--- docs/test/js/select.js
+++ docs/test/js/select.js
@@ -1,32 +1,47 @@
 'use strict';
 
-(function() {
-    const selectArrow = document.querySelectorAll('.ui-select-arrow')
-    const selectInputList = document.querySelectorAll('.ui-select .ui-input')
-    let select = null;
-    let selectList = null;
-    let selectInput = null;
+(function () {
+  var selectArrow = document.querySelectorAll('.ui-select-arrow');
+  var selectInputList = document.querySelectorAll('.ui-select .ui-input');
+  var select = null;
+  var selectList = null;
+  var selectInput = null;
 
-    function toggleSelect(elem) {
-        elem.classList.contains('active') ?  elem.classList.remove('active') :  elem.classList.add('active');
+  function toggleSelect(elem) {
+    elem.classList.contains('active') ? elem.classList.remove('active') : elem.classList.add('active');
+  }
+
+  function checkNewOption(event) {
+    selectList.forEach(function (el) {
+      return el.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    selectInput.value = event.target.textContent;
+    toggleSelect(select);
+  }
+
+  function showSelectList(event) {
+    select = event.target.parentElement;
+    selectInput = select.querySelector('.ui-input');
+    selectList = select.querySelectorAll('.ui-option');
+    toggleSelect(select);
+    selectList.forEach(function (el) {
+      return el.addEventListener('click', checkNewOption);
+    });
+  }
+
+  function onKeydown(keyCode) {
+    switch (keyCode) {
+      case 40:
+        console.dir(selectInput);
+        console.log(selectInput.nextSibling);
     }
+  }
 
-    function checkNewOption(event) {
-        selectList.forEach(el => el.classList.remove('active'))
-        event.target.classList.add('active');
-        selectInput.value =  event.target.textContent;
-        toggleSelect(select);
-    }
-
-    function showSelectList(event) {
-        select = event.target.parentElement;
-        selectInput = select.querySelector('.ui-input');
-        selectList = select.querySelectorAll('.ui-option');
-
-        toggleSelect(select);
-        selectList.forEach(el => el.addEventListener('click', checkNewOption));
-    }
-
-    selectArrow.forEach(el => el.addEventListener('click', showSelectList))
-    selectInputList.forEach(el => el.addEventListener('focus', showSelectList))
-})()
+  selectArrow.forEach(function (el) {
+    return el.addEventListener('click', showSelectList);
+  });
+  selectInputList.forEach(function (el) {
+    return el.addEventListener('focus', showSelectList);
+  });
+})();
